ece1b8f554116adbb9183bb0b9c5652208e9ca7b
M SHAH <shah@idk.uno>
2021-01-26 18:50:27 +0000

    first version ready

---
+++ src/models.ts
@@ -0,0 +1,117 @@
+/**
+ * A chat id ends with `@c.us` or `@g.us` for group chats.
+ * 
+ * Example:
+ * 
+ * A group chat: `"447123456789-1445627445@g.us"`
+ * A group chat: `"447123456789@g.us"`
+ * 
+ */
+export type ChatId = string;
+
+/**
+ * A group chat ends with `@g.us` and usually has two parts, the timestamp of when it was created, and the user id of the number that created the group. For example `[creator number]-[timestamp]@g.us`
+ * 
+ * Example:
+ * 
+ * `"447123456789-1445627445@g.us"`
+ */
+export type GroupChatId = string;
+
+/**
+ * A contact id ends with `@c.us` and only contains the number of the contact. For example, if the country code of a contact is `44` and their number is `7123456789` then the contact id would be `447123456789@c.us`
+ * 
+ * Example:
+ * 
+ * `"447123456789@c.us"`
+ */
+export type ContactId = string;
+
+/**
+ * The id of a message. The format is `[boolean]_[ChatId]_[random character strin]`
+ * 
+ * Example:
+ * 
+ * `"false_447123456789@c.us_9C4D0965EA5C09D591334AB6BDB07FEB"`
+ */
+export type MessageId = string;
+
+/**
+ * This is a generic type alias for the content of a message
+ * 
+ * Example:
+ * 
+ * `"hello!"`
+ */
+export type Content = string;
+
+/**
+ * 
+ * Data URLs, URLs prefixed with the data: scheme, allow content creators to embed small files inline in documents. They were formerly known as "data URIs" until that name was retired by the WHATWG.
+ * 
+ * 
+ * Data URLs are composed of four parts: a prefix (data:), a MIME type indicating the type of data, an optional base64 token if non-textual, and the data itself:
+ * 
+ * Example:
+ * `"data:[<mediatype>][;base64],<data>"`
+ * 
+ * Learn more here: https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/Data_URIs
+ */
+export type DataURL = string;
+
+/**
+ * Base64 is basically a file encoded as a string.
+ * 
+ * Base64 is a group of similar binary-to-text encoding schemes that represent binary data in an ASCII string format by translating it into a radix-64 representation. The term Base64 originates from a specific MIME content transfer encoding.
+ * 
+ * Learn more here: https://developer.mozilla.org/en-US/docs/Glossary/Base64
+ */
+export type Base64 = string;
+
+/**
+ * The relative or absolute path of a file
+ * 
+ * Learn more here: https://www.w3schools.com/html/html_filepaths.asp
+ */
+export type FilePath = string;
+
+export type StickerMetadata = {
+    /**
+     * The author of the sticker
+     * @default ``
+     */
+    author: string,
+    /**
+     * The pack of the sticker
+     * @default ``
+     */
+    pack: string
+  }
+
+  export type Mp4StickerConversionProcessOptions = {
+    /**
+     * Desired Frames per second of the sticker output
+     * @default `10`
+     */
+    fps?: number;
+    /**
+     * The video start time of the sticker
+     * @default `00:00:00.0`
+     */
+    startTime?: string;
+    /**
+     * The video end time of the sticker. By default, stickers are made from the first 5 seconds of the video
+     * @default `00:00:05.0`
+     */
+    endTime?: string;
+    /**
+     * The amount of times the video loops in the sticker. To save processing time, leave this as 0
+     * default `0`
+     */
+    loop?: number;
+    /**
+     * Centres and crops the video.
+     * default `true`
+     */
+    crop?: boolean;
+  }
---
+++ src/utils.ts
@@ -0,0 +1,55 @@
+import { DataURL } from "./models";
+import axios from 'axios';
+import isURL from 'validator/es/lib/isURL';
+
+/**
+ * A convinience method to download the [[DataURL]] of a file
+ * @param url The url
+ * @param optionsOverride You can use this to override the [axios request config](https://github.com/axios/axios#request-config)
+ * @returns Promise<DataURL>
+ */
+export async function getDUrl(url: string, optionsOverride: any = {}, returnBuffer : boolean = false){
+    try {
+      const res = await axios({
+          method:"get",
+          url,
+          headers: {
+            'DNT':1,
+            'Upgrade-Insecure-Requests':1
+          },
+          ...optionsOverride,
+          responseType: 'arraybuffer'
+        });
+      const dUrl : DataURL = `data:${res.headers['content-type']};base64,${Buffer.from(res.data, 'binary').toString('base64')}`;
+      return returnBuffer ? res.data : dUrl ;
+    } catch (error) {
+      console.log("TCL: getDUrl -> error", error)
+    }
+  }
+  
+  /**
+   * Use this to extract the mime type from a [[DataURL]]
+   */
+  export function base64MimeType(dUrl : DataURL) {
+    var result = null;
+    if (typeof dUrl !== 'string') {
+      return result;
+    }
+    var mime = dUrl.match(/data:([a-zA-Z0-9]+\/[a-zA-Z0-9-.+]+).*,.*/);
+    if (mime && mime.length) {
+      result = mime[1];
+    }
+    return result;
+  }
+
+  export const durlOrBase64ToBuffer = durl => Buffer.from(durl.replace(/^data:(image|video)\/(png|gif|jpeg|webp);base64,/,''), 'base64');
+
+  /**
+   * Check if a string is actually a DataURL
+   * @param s The string to check
+   */
+export const isDataURL = (s: string) => !!s.match(/^data:((?:\w+\/(?:(?!;).)+)?)((?:;[\w\W]*?[^;])*),(.+)$/g);
+
+export const isBase64 = (s: string) => /^([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/.test(s)
+
+export const isUrl = (s: string) => isURL(s)
